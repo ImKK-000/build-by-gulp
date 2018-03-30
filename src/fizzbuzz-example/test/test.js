7a300ac57e8127fe244ae23efa34ad699d122435
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-30 21:00:35 +0700

    Add {index.js, test.js} of fizzbuzz-example

---
+++ src/fizzbuzz-example/test/test.js
@@ -0,0 +1,61 @@
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const { sayFizzBuzz } = require('../index')
+
+describe('FizzBuzz Example', () => {
+  describe('sayFizzBuzz', () => {
+    it('Should 1 Expect 1', () => {
+      // Arrange
+      const data = 1
+      // Act
+      const result = sayFizzBuzz(data)
+      // Assert
+      expect(result).to.be.equal(1)
+    })
+
+    it('Should 3 Expect Fizz', () => {
+      // Arrange
+      const data = 3
+      // Act
+      const result = sayFizzBuzz(data)
+      // Assert
+      expect(result).to.be.equal('Fizz')
+    })
+
+    it('Should 5 Expect Buzz', () => {
+      // Arrange
+      const data = 5
+      // Act
+      const result = sayFizzBuzz(data)
+      // Assert
+      expect(result).to.be.equal('Buzz')
+    })
+
+    it('Should 6 Expect Fizz', () => {
+      // Arrange
+      const data = 6
+      // Act
+      const result = sayFizzBuzz(data)
+      // Assert
+      expect(result).to.be.equal('Fizz')
+    })
+
+    it('Should 10 Expect Buzz', () => {
+      // Arrange
+      const data = 10
+      // Act
+      const result = sayFizzBuzz(data)
+      // Assert
+      expect(result).to.be.equal('Buzz')
+    })
+
+    it('Should 15 Expect FizzBuzz', () => {
+      // Arrange
+      const data = 15
+      // Act
+      const result = sayFizzBuzz(data)
+      // Assert
+      expect(result).to.be.equal('FizzBuzz')
+    })
+  })
+})
